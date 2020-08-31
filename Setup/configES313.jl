660d74e400b1d3b096974ef133c59c067ab6e7eb
Bart De Clerck <declerck.bart@pm.me>
2020-08-31 19:17:29 +0200

    updated list of dependencies

--- Setup/configES313.jl
+++ Setup/configES313.jl
@@ -2,16 +2,33 @@
 
 using Pkg
 Pkg.update()
-Pkg.add("IJulia")
-Pkg.add("Plots")
+# General
+Pkg.add("Logging")
+Pkg.add("Dates")
+Pkg.add("Statistics")
 Pkg.add("Distributions")
-Pkg.add("LaTeXStrings")
-Pkg.add("StatsPlots")
-Pkg.add("Measures")
+Pkg.add("HypothesisTests")
+Pkg.add("CSV")
+Pkg.add("JLD")
 Pkg.add("JLD2")
-Pkg.add("SimJulia")
+# Plots
+Pkg.add("Plots")
+Pkg.add("StatsPlots")
+Pkg.add("LaTeXStrings")
+Pkg.add("Measures")
 Pkg.add(PackageSpec(url="https://github.com/BenLauwens/NativeSVG.jl"))
+# Optimisation
+Pkg.add("JuMP")
+Pkg.add("GLPK")
+Pkg.add("Optim")
+Pkg.add(PackageSpec(url="https://github.com/oxfordcontrol/GeneralQP.jl"))
+Pkg.add("NLopt")
+Pkg.add("Ipopt")
+# Discrete event simulation
+Pkg.add("ResumableFunctions")
+Pkg.add("SimJulia")
+# jupyterlab
+Pkg.add("IJulia")
 
-
-using IJulia
-jupyterlab()
+#using IJulia
+#jupyterlab()
