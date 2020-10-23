0e643b4ac282315b8e5518617990eb6470c5da95
Bart De Clerck <declerck.bart@pm.me>
2020-10-23 16:41:03 +0200

    created

---
+++ Exercises/PS08 - SimJulia (application).jl
@@ -0,0 +1,15 @@
+### A Pluto.jl notebook ###
+# v0.12.4
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ a1373212-153d-11eb-0716-d98194a1fbe4
+using SimJulia, Distributions, Plots
+
+# ╔═╡ ac736b76-153d-11eb-2dd8-f79d303626fa
+
+
+# ╔═╡ Cell order:
+# ╠═a1373212-153d-11eb-0716-d98194a1fbe4
+# ╠═ac736b76-153d-11eb-2dd8-f79d303626fa
