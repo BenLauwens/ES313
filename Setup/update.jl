32d54a91107143a225bdc9eb35480a4f3aac9fc6
Bart De Clerck <declerck.bart@pm.me>
2021-09-08 14:54:09 +0200

    added update script

---
+++ Setup/update.jl
@@ -0,0 +1,17 @@
+using Pkg
+# change pwd 
+cd(joinpath(dirname(@__FILE__),".."))
+# activate environment
+Pkg.activate(pwd())
+
+using GitCommand
+# fetch package updates
+Pkg.update()
+# fetch git updates
+git() do git
+    run(`$git config pull.rebase false`)
+    # if on CDN 
+    #run(`$git config --global http.proxy http://CDNusername:CDNpassword@dmzproxy005.idcn.mil.intra:8080`)
+    run(`$git pull`)
+end
+
