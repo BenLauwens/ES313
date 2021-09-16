e6bd516643d513360e0debcb44475e4fbdd534ac
Bart De Clerck <declerck.bart@pm.me>
2021-09-16 11:22:07 +0200

    added v1.5.3 vs v1.6 difference

--- Setup/update.jl
+++ Setup/update.jl
@@ -2,18 +2,24 @@ using Pkg
 # change pwd 
 cd(joinpath(dirname(@__FILE__),".."))
 # activate environment
+VERSION < v"1.6" ? (using GitCommand) : (using Git)
 Pkg.activate(pwd())
-
-using GitCommand
 # fetch package updates
 Pkg.update()
 # fetch git updates
-git() do git
-    # stash local changes
-    run(`$git stash`)
-    run(`$git config pull.rebase false`)
-    # if on CDN 
-    #run(`$git config --global http.proxy http://CDNusername:CDNpassword@dmzproxy005.idcn.mil.intra:8080`)
-    run(`$git pull`)
+if VERSION < v"1.6"
+    git() do git
+        # stash local changes
+        GitCommand.run(`$git stash`)
+        GitCommand.run(`$git config pull.rebase false`)
+        # if on CDN 
+        #run(`$git config --global http.proxy http://CDNusername:CDNpassword@dmzproxy005.idcn.mil.intra:8080`)
+        GitCommand.run(`$git pull`)
+    end
+else
+    Git.run(`$(git()) stash`)
+    Git.run(`$(git()) config pull.rebase false`)
+    Git.run(`$(git()) pull`)
 end
+
 
