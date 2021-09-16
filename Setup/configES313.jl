c2ec8f9dd568ffbf99515844ec53164f6aa42965
Bart De Clerck <declerck.bart@pm.me>
2021-09-16 11:21:41 +0200

    added v1.5.3 vs v1.6 difference

--- Setup/configES313.jl
+++ Setup/configES313.jl
@@ -3,9 +3,9 @@
 # ------------------------------------------------- #
 
 # set the proxy server setting if required (on CDN)
-ENV["HTTP_PROXY"] = "http://CDNUSER:CDNPSW@dmzproxy005.idcn.mil.intra:8080"
+#ENV["HTTP_PROXY"] = "http://CDNUSER:CDNPSW@dmzproxy005.idcn.mil.intra:8080"
 # set the path
-downloadfolder = joinpath(homedir(),"ES313")
+downloadfolder = joinpath(homedir(),"Documents")
 
 
 # ------------------------------------------------- #
@@ -16,24 +16,28 @@ downloadfolder = joinpath(homedir(),"ES313")
 using Pkg
 using Logging
 
-@info "Installing GitCommand..."
-Pkg.add("GitCommand")
+!ispath(downloadfolder) ? mkdir(downloadfolder) : nothing
+cd(downloadfolder)
 
-# download course git folder
-using GitCommand
-git() do git
-    !ispath(downloadfolder) ? mkdir(downloadfolder) : nothing
-    cd(downloadfolder)
-    @info "Downloading course material into $(downloadfolder)"
-    try
-        run(`$git clone https://github.com/BenLauwens/ES313.git`)
-        @info "Download complete"
-    catch err
-        @warn "Something went wrong, check one of the following:\n  - .gitignore file location\n  - destination folder already a git repository"
-        @info err
+@info "Installing Git tools for Julia $(VERSION)..."
+
+VERSION < v"1.6" ? (Pkg.add("GitCommand"), using GitCommand) : (Pkg.add("Git"), using Git)
+@info "Downloading course material into $(downloadfolder)"
+try
+    if VERSION < v"1.6"
+        GitCommand.git() do git
+            run(`$git clone https://github.com/BenLauwens/ES313.git`)
+        end
+    else
+        run(`$(git()) clone https://github.com/BenLauwens/ES313.git`)
     end
+    @info "Download complete"
+catch err
+    @warn "Something went wrong, check one of the following:\n  - .gitignore file location\n  - destination folder already a git repository"
+    @info err
 end
 
+ 
 # Install & download required packages into environment
 cd(joinpath(downloadfolder,"ES313"))
 Pkg.activate(".")
