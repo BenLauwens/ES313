b065d624fd86d4433a567117c26ec0da77704a48
Bart De Clerck <declerck.bart@pm.me>
2021-10-12 16:05:27 +0200

    update projects

--- Exercises/PS09 - Projects.jl
+++ Exercises/PS09 - Projects.jl
@@ -1,12 +1,121 @@
 ### A Pluto.jl notebook ###
-# v0.12.4
+# v0.16.1
 
 using Markdown
 using InteractiveUtils
 
+# ╔═╡ 023da279-7a42-4cb1-addb-613d1363a282
+md"""
+# 2021 project ideas:
+Below you can find a list of possible projects. Almost all of these are described in a very generic way and will require some research on your behalf. Some background and/or similar studies are provided for each project, most of these being more complex than what is expected of you.
+"""
+
+# ╔═╡ 42968b6f-a86f-48dd-b354-d06dfd53624c
+md"""
+## Swarming (multiple projects)
+The use of drone swarms has several applications in a military context. Drones can be equipped with different payloads such as high explosives, sensors (ISR), jammers, etc. Currently, all major military countries are conducting research on both the use of and the defense against drone swarms.
+
+### Possible topics:
+- **command and control (C2) of a drone swarm**: for the technology to be scalable, each element of the swarm should be cheap, so you will have a limited amount of control drones that are used for relaying communications with the HQ. The movement patterns of drones in different cases can be studied and optimised.
+- **use of a drone swarm for ISR purposes**: drone swarms can be used to [gather intelligence](https://www.timesofisrael.com/in-apparent-world-first-idf-deployed-drone-swarms-in-gaza-fighting/) in addition to more traditional assets. Given a specific scenario and an ISR capacity, determine optimal use cases.
+- **defense against a drone swarm**: given an adversarial drone swarm attack, determine optimal techniques of defense (weapon system and/or engagement method).
+- **attack using a drone swarm**:  given an adversial defense system, determine optimal techniques/patterns/tactics to maximise the impact of the attack.
+
+### References:
+* [General info](https://www.vifindia.org/article/2020/april/27/drone-swarms-bracing-up-with-the-new-threat)
+* [More details](http://unsworks.unsw.edu.au/fapi/datastream/unsworks:45320/SOURCE02?view=true)
+"""
+
+# ╔═╡ d3195ce3-cc34-458f-a526-f346591b34b2
+md"""
+## Mine hunting
+Mine Counter Measure (MCM) is a general capability that can include several types of missions required to complete a specific operational intent:
+- General or detailed survey to prepare Mine Warfare (MW) operations,
+- Surveillance or exploration to assess Mine Warfare risk,
+- Reconnaissance and Mine avoidance to avoid mine threat,
+- Clearance to sanitize,
+- Masking and jamming to prevent mine action,
+- Provocation and discredit to deceive mine logic.
+
+### Possible projects
+- **optimise sweep patterns to maximise detection**: the detection probability of a mine depends on its position with respect to the sonar and the composition of the seabed. Simulate a minefield and try to optimise the sweeping settings to finetune the detection rate.
+- **optimise mine field**: different types of (deep) sea mines exist. Given a set of mines, evaluate the composition/layout of a minefield for maximum effect.
+
+### References
+- [Synthetic Aperture and 3D Imaging for Mine Hunting Sonar](https://hal.archives-ouvertes.fr/hal-00504862/document)
+- [Minhunting sonar perfomance](https://apps.dtic.mil/sti/pdfs/ADA342568.pdf)
+- [Modern mines](https://cimsec.org/modern-naval-mines-not-your-grandfathers-weapons-that-wait/)
+"""
+
+# ╔═╡ 8bc9e310-3151-4c49-b009-04b4cc1a4a84
+md"""
+## Airfield destruction
+Destroying or rendering unusable an enemy airfield can have a major impact on the course of a conflict (think of the Six Day War). Typically, ballistic missiles and cruise missiles are used, each with a different effect and a different cost. A military airfield usually has a defense system against missiles and rockets where the interception rate is different for each weapon system. 
+### Possible project
+Consider an airport with its different components: the runway, the critical infrastructure (POL + control tower) and the different airplanes. You could look at Kleine Brogel or Florennes air base for inspiration. Determine the effect of using a specific set of weapon systems (ballistic & guided munitions, with submunitions or not with different CEP) and consider the optimal/ideal/minimal configuration that allows you to take out the airfield.
+
+### References
+* [Defense systems](https://www.boeing.com/defense/missile-defense/)
+"""
+
+# ╔═╡ 279eda92-e1cf-480e-a093-e17625046125
+md"""
+## Digital communication
+
+### Possible projects
+* **TCP/IP**: simulate the TCP/IP model for data transfer and look up the limits with respect to data troughput for given hardware.
+* **routing**: analyse how well routing works using different routing protocols on a (dynamic) network.
+* **tactical radio network**: compare different scheduling algorithms to maximise data throughput in a tactical radio network.
+
+### References
+* [Basic on internet and IP routing, TCP/IP](https://www.khanacademy.org/computing/computers-and-internet/xcae6f4a7ff015e7d:the-internet)
+* [Routing protocol](https://en.wikipedia.org/wiki/Routing_protocol)
+* [Scheduling](https://en.wikipedia.org/wiki/Scheduling_(computing)#Scheduling_disciplines)
+"""
+
+# ╔═╡ a7659bd3-796b-4155-b01f-1d9ef6b42f12
+
+
+# ╔═╡ 72df832c-442f-4693-9953-df873951a1db
+md"""
+## Energy grid management
+Managing the electricity grid is a major challenge. 
+### Possible projects
+
+### References
+- [Introduction to electricity markets](https://www.e-education.psu.edu/ebf483/node/816)
+"""
+
+# ╔═╡ 65c30d33-de0e-410d-afa9-9e0e84431012
+md"""input ben:
+- risicomodellen gecombineerd met discreet event + bayesiaans (covid + splitsen)
+- 
+"""
+
+# ╔═╡ 8a23e486-5f4e-4db3-8955-ac5a948203f1
+md"""
+## General topic
+
+### Possible projects
+
+### References
+"""
+
+# ╔═╡ 79db607a-8af7-4f60-a339-0e27cd6edf1f
+md"""
+## General topic
+
+### Possible projects
+
+### References
+"""
+
+# ╔═╡ 3117a9ed-4577-47fb-bc7f-54fb9572e313
+
+
 # ╔═╡ 1e897cf4-18f5-11eb-2953-d1d83d41a044
 md"""
-# Project ideas:
+# 2020 Project ideas:
 Below you can find a list of possible projects. Almost all of these are described in a very generic way and will require some research on your behalf. Some background and/or similar studies are provided for each project, most of these being more complex than what is expected of you.
 
 ## Urban planning (multiple projects) (up to 3 projects)
@@ -199,4 +308,15 @@ You can always come to use with your own project proposition.
 """
 
 # ╔═╡ Cell order:
+# ╟─023da279-7a42-4cb1-addb-613d1363a282
+# ╟─42968b6f-a86f-48dd-b354-d06dfd53624c
+# ╟─d3195ce3-cc34-458f-a526-f346591b34b2
+# ╟─8bc9e310-3151-4c49-b009-04b4cc1a4a84
+# ╟─279eda92-e1cf-480e-a093-e17625046125
+# ╠═a7659bd3-796b-4155-b01f-1d9ef6b42f12
+# ╠═72df832c-442f-4693-9953-df873951a1db
+# ╠═65c30d33-de0e-410d-afa9-9e0e84431012
+# ╠═8a23e486-5f4e-4db3-8955-ac5a948203f1
+# ╠═79db607a-8af7-4f60-a339-0e27cd6edf1f
+# ╠═3117a9ed-4577-47fb-bc7f-54fb9572e313
 # ╟─1e897cf4-18f5-11eb-2953-d1d83d41a044
