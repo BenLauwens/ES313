5c32614de0da9dd5d2b36546cb80eee749923c0c
Ben Lauwens <ben.lauwens@gmail.com>
2022-08-29 10:25:01 +0200

    Updates 2022-2023

--- Lectures/Lecture00.jl
+++ Lectures/Lecture00.jl
@@ -54,39 +54,39 @@ md"""## Schedule
 
 ### Theory
 
-- 31/08: Cellular Automaton + Game of Life
-- 01/09: Year Coord
-- 14/09: Physical Modelling + Self-Organization
-- 21/09: Optimisation Techniques
-- 22/09: Linear Programming I
-- 28/09: Linear Programming II + Applications I
-- 29/09: Applications of Linear Programming II
-- 19/10: Introduction to Discrete Event Simulation
-- 20/10: Process Driven DES: SimJulia I
-- 26/10: Process Driven DES II + Applications I
-- 27/10: Applications with SimJulia II
+- 30/08: Cellular Automaton + Game of Life
+- 31/08: Year Coord
+- 07/09: Physical Modelling
+- 14/09: Self-Organization
+- 20/09: Optimisation Techniques
+- 21/09: Linear Programming I
+- 28/09: Linear Programming II
+- 05/10: Applications of Linear Programming II
+- 11/10: Introduction to Discrete Event Simulation
+- 12/10: Process Driven DES: SimJulia I
+- 25/10: Process Driven DES II + Applications I
+- 26/10: Applications with SimJulia II
 
 ### Practice
 
-- 07/09: Visualisation
-- 08/09: Cellular Automaton
-- 15/09: Game of Life
-- 05/10: Physical Modelling + Self-Organization
-- 06/10: Optimisation Techniques I
-- 12/10: Optimisation Techniques II + Linear Programming I
-- 13/10: Linear Programming II
-- 09/11: Introduction to Discrete Event Simulation
-- 10/11: Process Driven DES: SimJulia I
-- 16/11: Process Driven DES II + Applications with SimJulia I
-- 17/11: Applications with SimJulia II
-- 24/11: Performance
+- 06/09: Visualisation
+- 13/09: Cellular Automaton + Game of Life
+- 27/09: Physical Modelling + Self-Organization
+- 04/10: Optimisation Techniques I
+- 18/10: Optimisation Techniques II + Linear Programming I
+- 19/10: Linear Programming II
+- 08/11: Introduction to Discrete Event Simulation
+- 09/11: Process Driven DES: SimJulia I
+- 15/11: Process Driven DES II + Applications with SimJulia I
+- 16/11: Applications with SimJulia II
+- 23/11: Performance
 
 ### Project
 
-- 09/11: List of projects available
+- 08/11: List of projects available
 - we are available during contact hours
-- 23/11: obligatory meeting: understanding of the problem
-- 07/12: obligatory meeting: progress"""
+- 22/11: obligatory meeting: understanding of the problem
+- 06/12: obligatory meeting: progress"""
 
 # ╔═╡ 0b3e8cf2-eb93-11ea-140d-35c9f1074ea9
 md"""## Evaluation
@@ -100,9 +100,6 @@ Examen: Project with Oral Defense
 - Optimisation Techniques
 - Linear Programming
 - Discrete Event Simulation"""
-
-# ╔═╡ 9891912e-e419-4d54-8b40-a31a61b23d49
-
 
 # ╔═╡ 5fe3a710-ebbe-11ea-3987-3fec2c7f3057
 md"""## Julia
@@ -124,5 +121,4 @@ $(Markdown.parse.(readlines("./Setup/readme.md"),flavor=:julia))
 # ╟─29d92270-eb92-11ea-374f-45b11865dd9a
 # ╟─ee8e57c0-eb92-11ea-193a-5bfcf7800d99
 # ╟─0b3e8cf2-eb93-11ea-140d-35c9f1074ea9
-# ╠═9891912e-e419-4d54-8b40-a31a61b23d49
 # ╟─5fe3a710-ebbe-11ea-3987-3fec2c7f3057
